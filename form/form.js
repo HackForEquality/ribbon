979c6111bac8ce372d5451e1164243ce746a32a1
Miles McGuire <miles@milesmcguire.com>
2015-03-28 18:54:29 +0000

    form changes

--- form/form.js
+++ form/form.js
@@ -1,17 +1,24 @@
 function updateFormURL () {
     var inputs = document.querySelectorAll(".ribbons input");
     inputs = Array.prototype.slice.call(inputs);
-    var selectedInputs = inputs.filter(function (element) {
+    var template_parts = inputs.filter(function (element) {
         return element.checked;
-    })
-    var selectedInputsValues = selectedInputs.map(function (element) {
-        return element.value;
-    })
-    var concatValues = selectedInputsValues.reduce(function (acc, el) {
-        return acc += el;
-    }, "");
-    window.concatValeus = concatValues;
-    document.getElementById("output_value").innerHTML = concatValues;
+    }).map(function (element) {
+        return element.value.split('_');
+    });
+    template_parts = [].concat.apply([], template_parts);
+    var color_spans = Array.prototype.slice.call(document.getElementsByClassName("template-color"));
+    var vertical_spans = Array.prototype.slice.call(document.getElementsByClassName("template-vertical"));
+    var horizontal_spans = Array.prototype.slice.call(document.getElementsByClassName("template-horizontal"));
+    color_spans.forEach(assignValue(template_parts[0]));
+    vertical_spans.forEach(assignValue(template_parts[1]));
+    horizontal_spans.forEach(assignValue(template_parts[2]));
+}
+
+function assignValue (value) {
+    return function assignValueHelper (element) {
+        element.innerHTML = value;
+    }
 }
 
 ready(updateFormURL);
